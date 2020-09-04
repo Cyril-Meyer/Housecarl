d4eacb59e3c96323c48bbe4dbd24fa5535819cc9
Cyril-Meyer <contact@cyrilmeyer.eu>
2020-09-04 02:18:34 +0200

    add html.h (contain fixed or flagged HTML response strings)

---
+++ html.h
@@ -0,0 +1,25 @@
+extern const char html_403_forbidden[] PROGMEM = R"(
+<!DOCTYPE html>
+<html>
+<head>
+<title>403 Forbidden</title>
+</head>
+<body>
+<h1>Forbidden</h1>
+</body>
+</html>
+)";
+
+extern const char html_ls[] PROGMEM = R"(
+<!DOCTYPE html>
+<html>
+<head>
+<title>ls</title>
+</head>
+<body>
+<h1>ls</h1>
+<h2>%%CIID%%<h2>
+<h2>%%CLID%%<h2>
+</body>
+</html>
+)";
